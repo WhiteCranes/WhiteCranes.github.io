7878d4b920ddaf6613cf2cd9607ccc1bcdda7152
WhiteCranes <30688353+WhiteCranes@users.noreply.github.com>
2023-09-08 17:27:38 +0800

    Update new.js

--- js/new.js
+++ js/new.js
@@ -607,7 +607,7 @@ function sendx(a) {
         return;
     }
     if (a) {
-        fetch(a);
+        fetch(server+a);
     }
 }
 
@@ -653,12 +653,12 @@ function sendz(a, div, url) {
         if (empty) {
             addmessage("left", "发生错误请重试", false, false);
             main.scrollTop = main.scrollHeight;
-            fetch('direct1.php?reset');
+            fetch(server+'direct1.php?reset');
         }
         if (nofinish) {
             addmessage("left", "作答被魔法打断", false, false);
             main.scrollTop = main.scrollHeight;
-            fetch('direct1.php?reset');
+            fetch(server+'direct1.php?reset');
         }
         console.log("%c 作答完成", " text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:5em")
         const elementsToRemove = document.querySelectorAll('.qblinking');
