a244c91ae3294fff921f4dcb2a28eb95b32cfba4
WhiteCranes <30688353+WhiteCranes@users.noreply.github.com>
2024-02-16 14:10:16 +0800

    Update new.js

--- js/new.js
+++ js/new.js
@@ -738,11 +738,11 @@ function sendx(a) {
 function sendz(a, div, url) {
     let buff = "";
   let socket;
-    if (window.location.protocol === 'http:') {
-      socket = new WebSocket(url);
-    }else{
-      addmessage("left", `<p>当前为https，无法正常使用，请修改浏览器地址栏为http，把s去掉。</p>`, false, true);
-    }
+    // if (window.location.protocol === 'http:') {
+    //   socket = new WebSocket(url);
+    // }else{
+    //   addmessage("left", `<p>当前为https，无法正常使用，请修改浏览器地址栏为http，把s去掉。</p>`, false, true);
+    // }
     
     var empty = true;
     var nofinish = true;
